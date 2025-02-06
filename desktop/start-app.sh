f5af636254b96a77209a607bc0c287213340d58a
Geoff Dutton <geoff.dutton@noaa.gov>
2025-02-06 22:18:00 +0000

    ports changed

--- desktop/start-app.sh
+++ desktop/start-app.sh
@@ -5,4 +5,8 @@ source /home/ucats/code/.venv/bin/activate
 
 cd /home/ucats/code
 
+# start telem (comment out if not needed)
+/home/ucats/code/.venv/bin/python /home/ucats/code/telem.py &
+
+# start instrument
 /home/ucats/code/.venv/bin/python /home/ucats/code/instrument.py >> /home/ucats/code/ucats-b.log 2>&1
