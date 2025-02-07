f97dea35b193ed77c6d7c10c426c187f43d0dc46
Geoff Dutton <geoff.dutton@noaa.gov>
2025-02-07 01:02:45 +0000

    update

--- desktop/start-telem.sh
+++ desktop/start-telem.sh
@@ -5,7 +5,7 @@ source /home/ucats/code/.venv/bin/activate
 
 cd /home/ucats/code
 
-sleep 10
+sleep 30
 
 # start instrument
 /home/ucats/code/.venv/bin/python /home/ucats/code/telem.py >> /home/ucats/code/telem.log 2>&1
